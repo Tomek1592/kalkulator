d9bf9a309464b8101f54fc7a462623264d8f62d9
tomek1592 <tomek1592@gmail.com>
2020-08-29 20:34:02 +0200

    a

--- src/components/Savings/Savings.tsx
+++ src/components/Savings/Savings.tsx
@@ -1,10 +1,10 @@
-import React, { FC } from 'react';
+import React from 'react';
 
 import { Row, Col } from 'antd';
 
 import SavingsForm from '../SavingsForm/SavingsForm';
 
-const Savings: FC = () => {
+const Savings = (): JSX.Element => {
   return (
     <Row align="middle" justify="center">
       <Col xs={24} sm={22} md={18} lg={12} xl={12} xxl={12}>
